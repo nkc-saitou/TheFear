cf45ec61941a683eb44d1cbdebfbff4e3c3391b7
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2017-05-31 16:24:44 +0900

    はじめてコミット

---
+++ TheFear/TheFear/Assets/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    [SerializeField,Range(-20,50)]
+    float speed = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+	}
+}

845ff2aeeb1338ddd654b79c165d0ffb50e348d8
shigeyama <nkc.wataru.shigeyama@gmail.com>
2017-07-05 20:04:36 +0900

    マネキン

---
+++ TheFear/TheFear/Assets/saitou/mannequin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mannequin : MonoBehaviour
+{
+
+    //--------------------------------------
+    // public
+    //--------------------------------------
+
+    public GameObject manequinsAdd; //マネキン用
+
+    //--------------------------------------
+    // private
+    //--------------------------------------
+
+    int currentWave = 0; //現在のウェーブ
+    public List<GameObject> manequinLis = new List<GameObject>();
+
+    void Start()
+    {
+        //全部隠す
+        foreach (Transform t in manequinsAdd.transform)
+        {
+            manequinLis.Add(t.gameObject);
+            t.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if(hit.tag == "Player" && manequinLis.Count > currentWave)
+        {
+            //ウェーブを増やす
+            manequinLis[currentWave].SetActive(true);
+            currentWave++;
+        }
+    }
+}

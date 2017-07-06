11a122e7aa3634b660a0bad901de7bee6a97b576
DeNChi002 <tyoudennjisyottogann@yahoo.co.jp>
2017-07-06 16:02:01 +0900

    修正

--- TheFear/TheFear/Assets/shigeya/FallEthan.cs
+++ TheFear/TheFear/Assets/shigeya/FallEthan.cs
@@ -15,6 +15,10 @@ public class FallEthan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            FallEthanFlg();
+        }
     }
 
     void FallEthanFlg()
